004dd806bff0465376c89ec7ad220da454cf75c4
start.vaadin.com <no-reply@vaadin.com>
2023-09-29 08:11:43 +0000

    Generated project

---
+++ src/main/java/com/d4rkr0n1n/essentials/Application.java
@@ -0,0 +1,24 @@
+package com.d4rkr0n1n.essentials;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "theessentials", variant = Lumo.DARK)
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
